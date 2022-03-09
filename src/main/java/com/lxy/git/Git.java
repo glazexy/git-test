ec962c19e065986b91645d18045a5f1bbb3e74df
glaze <100449682@qq.com>
2022-03-09 15:03:25 +0800

    third commit

--- src/main/java/com/lxy/git/Git.java
+++ src/main/java/com/lxy/git/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("liuxinying git1");
         System.out.println("liuxinying git2");
+        System.out.println("liuxinying git3");
     }
 }
