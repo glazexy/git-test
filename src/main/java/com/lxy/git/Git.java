29610b701c35b6c908bd0496fdd08ce852c744ed
glaze <100449682@qq.com>
2022-03-09 15:03:03 +0800

    second commit

--- src/main/java/com/lxy/git/Git.java
+++ src/main/java/com/lxy/git/Git.java
@@ -3,5 +3,6 @@ package com.lxy.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("liuxinying git1");
+        System.out.println("liuxinying git2");
     }
 }
